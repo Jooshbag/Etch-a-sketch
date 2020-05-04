392d9821d4fd12b4548742abc3ef20bbb5cc3649
Josh <jdanielson04@gmail.com>
2020-05-04 13:13:54 -0700

    Created the rest button with prompt

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,13 +1,22 @@
-var amount = 256;
 
 
-for(x=0; x<amount; x++) {
+var amount = 16;
+var blocks = amount * amount;
+
+createGrid();
+
+function createGrid(){ 
+for(x=0; x<blocks; x++) {
     const sketchDiv = document.createElement('div');
     sketchDiv.setAttribute('id', 'sketchDiv');
     sketchDiv.addEventListener('mouseover', changeColor);
+
+    const resetButton = document.getElementById('reset');
+    resetButton.addEventListener('click', resetColor);
     
     document.getElementById('sketchGrid').appendChild(sketchDiv);
     console.log('added block');
+}
 }
 
 //Creating hover over effect
@@ -16,3 +25,15 @@ function changeColor(e) {
     const cell = event.target;
     cell.style.background = 'black';
 }
+
+
+
+function resetColor() {
+    var sketchNode = document.querySelectorAll("#sketchDiv");
+    var i;
+    for (i = 0; i < sketchNode.length; i++) {
+      sketchNode[i].style.background = "white";
+    }
+    amount = prompt('How many blocks?');
+    createGrid();
+  }
